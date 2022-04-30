3a39280d36d62062a472a13f83eb3823a5c7f2b2
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-04-30 15:09:33 -0600

    initial commit

---
+++ demos.py
@@ -0,0 +1,7 @@
+
+allbooks = [{'title': 'Natalie', 'author': 'Sean', 'rating': '5'}, {'title': 'Sean', 'author': 'Natalie', 'rating': '4'}]
+
+
+
+for book in allbooks:
+    print(book['title'], book['author'])
