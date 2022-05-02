47f0f3f58a9be522a844fc4ef81ea7b1c5e1bde0
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-05-02 10:38:51 -0600

    able to update and delete from database

--- demos.py
+++ demos.py
@@ -1,7 +1,28 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import all_
 
-allbooks = [{'title': 'Natalie', 'author': 'Sean', 'rating': '5'}, {'title': 'Sean', 'author': 'Natalie', 'rating': '4'}]
+app = Flask(__name__)
+
+# allbooks = [{'title': 'Natalie', 'author': 'Sean', 'rating': '5'}, {'title': 'Sean', 'author': 'Natalie', 'rating': '4'}]
 
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-book-collection.db'
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-for book in allbooks:
-    print(book['title'], book['author'])
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), unique=True, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    
+db.create_all()
+
+data = Book.query.all()
+book = db.session.query(Book).filter(Book.title=='Potter').first()
+# print(data[0].title)
+print(book)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import all_, delete
 
 
 app = Flask(__name__)
@@ -15,16 +16,13 @@ db = SQLAlchemy(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 class Book(db.Model):
-    
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     author = db.Column(db.String(250), unique=True, nullable=False)
     rating = db.Column(db.Float, nullable=False)
 
-    def __repr__(self):
-        return f'<Book {self.title}>'
     
-# db.create_all()
+db.create_all()
 
 # book1 = Book(id=1, title='Harry Potter and the Sorcering Wizardry Stones', author='Conservative Heroine, J.K. Rowling', rating=9.5)
 
@@ -36,35 +34,43 @@ all_books = []
 
 @app.route('/')
 def home():
-
+    all_books = Book.query.all()
     return render_template('index.html', all_books=all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
-    if request.method == "POST":
-        # dict = {}
-        # dict['title'] = request.form['title']
-        # dict['author'] = request.form['author']
-        # dict['rating'] = request.form['rating']
-        # all_books.append(dict)
+    if request.method == "POST":      
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        db.session.add(new_book)
+        db.session.commit()
         
-        new_book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        all_books.append(new_book)
-        
-        print(all_books)
         return redirect(url_for('home'))
     return render_template('add.html')
 
 
-@app.route('/edit_rating/<book_id>')
+@app.route('/edit_rating/<book_id>', methods=["GET", "POST"])
 def edit_rating(book_id):
-    
-    return render_template('edit_rating.html',  sddfdsx)
+    if request.method == "POST":
+        new_rating = request.form['new_rating']
+        book = db.session.query(Book).filter(Book.id==book_id).first()
+        book.rating = new_rating
+        db.session.commit()
+        return redirect(url_for('home'))
+    book = db.session.query(Book).filter(Book.id==book_id).first()
+    # print(book)
+    return render_template('edit_rating.html', book=book)
+
+@app.route('/delete/<book_id>')
+def delete(book_id):
+    book = Book.query.get(book_id)
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
